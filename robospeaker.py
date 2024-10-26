33455121cf7aaee2fb13a43031ad4b1678bb8ff3
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-10-26 22:27:02 +0600

    Update robospeaker.py

--- robospeaker.py
+++ robospeaker.py
@@ -1,11 +1,14 @@
 import pyttsx3
-if __name__=="__main__":
-    print("Welcome to Robo Speaker".center(50))
-    while True:
-        x = input("Enter what do you want to pronounce: ")
-        eng = pyttsx3.init()
-        eng.say(x)
-        eng.runAndWait()
 
-        if x == "q":
-                break
+if __name__== "__main__": #Making sure the code inside runs directly without referring to other py files
+    print("________________________Welcome________________________".center(50))
+    print("-------------------------------------------------------")
+    while True: #loops keep going as long as we don't press q
+        x = input("Enter what do you want to say ('q' to break): ")
+        eng = pyttsx3.init() #use init to convert the text to speech
+        eng.say(x) #pronounce the scripts
+        eng.runAndWait() 
+        #wait until the speech engine finishes speaking the input text before moving on next command
+
+        if x == "q": #breaks the loop
+            break
