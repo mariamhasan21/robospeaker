400cb5601f1ca9666897a21a139959b97de4826c
Mariam Hasan <86767403+mariamhasan21@users.noreply.github.com>
2024-11-02 11:03:22 +0600

    Update robospeaker.py

--- robospeaker.py
+++ robospeaker.py
@@ -1,14 +1,15 @@
 import pyttsx3
 
 if __name__== "__main__": #Making sure the code inside runs directly without referring to other py files
-    print("________________________Welcome________________________".center(50))
+    print("------------------------Welcome------------------------".center(50))
     print("-------------------------------------------------------")
-    while True: #loops keep going as long as we don't press q
-        x = input("Enter what do you want to say ('q' to break): ")
-        eng = pyttsx3.init() #use init to convert the text to speech
+   
+    while True: #loops keep going as long as we don't press 'q'
+        x = input("Enter what do you want to pronounce ('q' to break): ")
+        eng = pyttsx3.init() #Init is like an engine that converts energy from one source to another. So, init converts the text to speech
         eng.say(x) #pronounce the scripts
         eng.runAndWait() 
-        #wait until the speech engine finishes speaking the input text before moving on next command
+        #waits until the speech engine finishes speaking the input text before moving on to the next command
 
         if x == "q": #breaks the loop
             break
